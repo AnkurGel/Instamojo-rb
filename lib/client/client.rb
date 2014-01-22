7cba49a43bc91ed62b26d2661c0213aee00cc7a4
Ankur Goel <ankurgel@gmail.com>
2014-01-22 21:41:06 +0530

    Added authentication, few GET & POST requests to talk to API - Major commit

--- lib/client/client.rb
+++ lib/client/client.rb
@@ -13,16 +13,13 @@ module Instamojo
     end
 
     def initialize(api)
-      #@conn = Faraday.new(Instamojo::URL, &@connection_options)
-      @conn = Faraday.new("https://instamojo.com") do |c|
-        c.request :url_encoded
-        c.response :logger
-        c.adapter Faraday.default_adapter
-      end
-      @conn.headers = @conn.headers.merge({"X-App-Id" => api.app_id})
+      @conn = Faraday.new(Instamojo::URL, &connection_options)
+
+      #To abstract in /errors.rb
+      raise "Supply API with app_id before generating client" unless api.app_id
+
       @app_id = api.app_id
-      #add_header(:accept, 'application/json')
-      #add_header("X-App-Id", @app_id)
+      add_header("X-App-Id", @app_id)
       @authentication_flag = "Not authenticated"
     end
 
@@ -34,23 +31,74 @@ module Instamojo
       @request = request
       sanitize_request
       @response = @conn.get(Instamojo::PREFIX + @request, params)
-      begin
-        JSON.parse(@response.body)
-        {:response_code => @response.status}.merge(JSON.parse(@response.body)) if @response.status != 200
-      rescue JSON::ParserError
-        {:message => "Something went wrong. Expected JSON"}
-      end
+      return sanitize_response
     end
 
-    def post(request, params)
+    def post(request, params = {})
       @request = request
       sanitize_request
       @response = @conn.post(Instamojo::PREFIX + @request, params)
-      JSON.parse(@response.body)
+      sanitize_response
     end
 
 
-    def authenticate(username, password)
+    #POST /auth/
+    #Authenticate, generate token and add header
+    def authenticate(username = nil, password = nil, options = {}, &block)
+      if username.is_a?(Hash) or password.is_a?(Hash)
+        options = username.is_a?(Hash) ? username : password
+      end
+      options["username"] = username if username and !username.is_a?Hash
+      options["password"] = password if password and !password.is_a?Hash
+
+
+      if block_given?
+        block_params = OpenStruct.new
+        block.call(block_params)
+        options = options.merge(block_params.marshal_dump)
+      end
+      #to tackle collective hash like:
+      #{"username" => "foo", :username => "foo"}
+      #added non-recursive basic code in lib/utility.rb
+      options.symbolize_keys!
+
+      #Raise error if doesn't find username and password key in options
+      @response = post('auth', options)
+      if @response.has_key?("success") and @response['success']
+        add_header("X-Auth-Token", @response['token'])
+      end
+      @response
+    end
+
+
+    #GET /offer - List all offers
+    def get_offers
+      unless get_connection_object.headers.has_key?("X-Auth-Token")
+        raise "Please authenticate() to see your offers"
+      end
+      get('offer')
+    end
+
+    #GET /offer/:slug
+    def get_offer(slug)
+      get("offer/#{slug}")
+    end
+
+
+    #POST /offer/ - Create an offer
+    def create_offer(options = {}, &block)
+      if block_given?
+        block_params = OpenStruct.new
+        block.call(block_params)
+        options = options.merge(block_params.marshal_dump)
+      end
+      options.symbolize_keys!
+      @response = post('offer', options)
+    end
+
+
+    #DELETE /offer/:slug - Archives an offer
+    def delete_offer(slug)
 
     end
 
@@ -68,12 +116,19 @@ module Instamojo
       @request.prepend('/') unless request.start_with? "/"
     end
 
-    def generate_token(username, password)
-      #Do POST request on /auth and extract token on successful authorization
-      response = @conn.post('/auth', {:username => username,
-                                      :password => password
-      })
-
+    def sanitize_response
+      if @response.status == 200
+        @authentication_flag = "Authenticated"
+        JSON.parse(@response.body)
+      else
+        begin
+          ({:client_error => "Something went wrong",
+            :response_code => @response.status
+          }).merge(JSON.parse(@response.body))
+        rescue JSON::ParserError
+          {:response_code => @response.status}
+        end
+      end
     end
 
     def add_header(key, value)
