e02f6b469b204f213eb5797cdf8106a5d1a3dd7d
Ankur Goel <ankurgel@gmail.com>
2014-01-22 20:13:50 +0530

    Added support to accept options hash and a block for API

--- lib/API/api.rb
+++ lib/API/api.rb
@@ -2,8 +2,16 @@ module Instamojo
   class API
     attr_reader :app_id
 
-    def initialize(app_id)
+    def initialize(app_id = nil, options = {})
+      options = app_id if app_id.is_a? Hash
+
       @app_id = app_id
+
+      options.each do |key, value|
+        instance_variable_set("@#{key}", value)
+      end
+
+      yield self if block_given?
     end
 
     def client
