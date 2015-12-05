9a5cddb4e773db78915a2da6b9c7e9764aa82d78
Ankur Goel <ankurgel@gmail.com>
2015-12-05 16:55:34 +0530

    added base.rb for basic top level Instamojo module skelton

--- lib/Instamojo-rb.rb
+++ lib/Instamojo-rb.rb
@@ -2,11 +2,7 @@ require 'faraday'
 require 'json'
 require 'uri'
 require 'ostruct'
+require_relative 'base'
 require_relative 'API/api'
 require_relative 'client/client'
 require_relative 'utility'
-
-module Instamojo
-  URL = "https://www.instamojo.com"
-  PREFIX = "/api/1"
-end
---
+++ lib/base.rb
@@ -0,0 +1,8 @@
+module Instamojo
+  HOST = "https://www.instamojo.com"
+  API_VERSION = "1.1"
+
+  class API
+
+  end
+end
